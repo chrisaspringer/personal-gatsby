b8559f4eb940d47b149c81249c8ad18ceb91ccbe
Chris Springer <chris.springer@akqa.com>
2019-07-10 11:14:09 -0400

    Add plugins for markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Chris Springer - Technologist`,
+    description: `Chris Springer's personal site. Chris is a technologist living in the Washington D.C. area.`,
+    author: `@chrisaspringer`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,14 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/star-512.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
